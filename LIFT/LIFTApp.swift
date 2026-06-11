a6d6f4055d2ab2fb18cd2eb71c528317566eff14
niharcv <niharaccs@gmail.com>
2026-06-10 20:58:57 -0400

    Initial Commit

---
+++ LIFT/LIFTApp.swift
@@ -0,0 +1,17 @@
+//
+//  LIFTApp.swift
+//  LIFT
+//
+//  Created by Nihar Vaidya on 6/10/26.
+//
+
+import SwiftUI
+
+@main
+struct LIFTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LIFTTests/LIFTTests.swift
@@ -0,0 +1,17 @@
+//
+//  LIFTTests.swift
+//  LIFTTests
+//
+//  Created by Nihar Vaidya on 6/10/26.
+//
+
+import Testing
+@testable import LIFT
+
+struct LIFTTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
